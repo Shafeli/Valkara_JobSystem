3dcbf91c4279724d62b5a6a10e11dab0fb8b1206
shafeli <silashafeli@gmail.com>
2025-06-19 00:48:51 -0700

    Job System work

--- Valkara_JobSystemProtoType/Source/JobSystemProtoType/Job.h
+++ Valkara_JobSystemProtoType/Source/JobSystemProtoType/Job.h
@@ -1,13 +1,42 @@
 #pragma once
 
 
+struct JobMetaData
+{
+	// TODO: ID system for Jobs
+	std::atomic<int>* counter = nullptr;
+	std::chrono::steady_clock::time_point startTime;
+	std::chrono::steady_clock::time_point endTime;
+};
 
 struct Job
 {
-private:
-	// profiler variables and other internal data can be added here
-
-public:
 	std::function<void()> Task; // The task to be executed by the job
+	std::shared_ptr<JobMetaData> Metrics;
+};
 
+struct JobHandle
+{
+	std::shared_ptr<std::atomic<int>> Counter;
+	std::shared_ptr<JobMetaData> Metrics;
+
+	[[nodiscard]] std::chrono::duration<double, std::milli> Duration() const
+	{
+		return Metrics ? Metrics->endTime - Metrics->startTime : std::chrono::milliseconds(0);
+	}
+
+	// Checking if the task has completed
+	[[nodiscard]] bool IsDone() const
+	{
+		return Counter && Counter->load(std::memory_order_acquire) == 0;
+	}
+
+
+	void BusyWait() const
+	{
+		while (Counter && Counter->load(std::memory_order_acquire) > 0) 
+		{
+			std::this_thread::yield();
+		}
+	}
 };
--- Valkara_JobSystemProtoType/Source/JobSystemProtoType/JobSystemProtoType.h
+++ Valkara_JobSystemProtoType/Source/JobSystemProtoType/JobSystemProtoType.h
@@ -15,9 +15,39 @@ public:
 
 	}
 
-	void Submit(const Job& work)
+	JobHandle Submit(std::function<void()> work)
 	{
-		// TODO: Implement job submission logic
+        auto counter = std::make_shared<std::atomic<int>>(1);
+        auto meta = std::make_shared<JobMetaData>();
+        meta->counter = counter.get();
+
+        Job job;
+
+        job.Task = 
+            [func = std::move(work), meta]()
+		{
+            meta->startTime = std::chrono::steady_clock::now(); // Start profile timer
+
+            try 
+            {
+                func(); // Do the real work
+            }
+            catch (...) 
+            {
+                // TODO: log or handle
+                throw;
+            }
+
+            meta->endTime = std::chrono::steady_clock::now(); // End profile timer
+
+           // --(*meta->counter); // notify completion
+            meta->counter->fetch_sub(1, std::memory_order_release); // More explicit and gives you memory ordering control
+		};
+
+        job.Metrics = meta;
+
+        m_threadPool.Enqueue(job.Task);
+        return JobHandle{ counter, meta };
 	}
 };
 
